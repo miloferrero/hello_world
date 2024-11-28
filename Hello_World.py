1ccc7ae9293690b7aa5df5de32cf6ef077d8c08d
miloguita_ok <emilio.ferrero@mercadolibre.com>
2024-11-28 11:58:08 -0300

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,20 @@
-print("Hello World")
+def lambda_handler(event, context):
+    """
+    AWS Lambda function handler.
+
+    Args:
+    - event: The event data that triggers the Lambda function.
+    - context: Information about the invocation, function, and runtime environment.
+
+    Returns:
+    - dict: A response object containing a status code and a message.
+    """
+    print("Hello, Militooo! The Lambda function was triggered.")
+    
+    # Example response
+    response = {
+        "statusCode": 200,
+        "body": "Hello, World! Welcome to AWS Lambda!"
+    }
+    
+    return response
